7bf5f4e082fa193d99a3e079ae85062aacf91c05
Martin-zhz1994 <martinzh94@hotmail.com>
2015-11-25 13:07:07 +0800

    确认完成和删除任务

--- Task.jsx
+++ Task.jsx
@@ -6,9 +6,33 @@ Task = React.createClass({
 		 //React.PropTypes声明了一系列的校验确保我们接收的数据是合法的
 		task:React.PropTypes.object.isRequired
 	},
+	toggleChecked(){
+		Tasks.update(this.props.task._id,{
+			$set:{checked: ! this.props.task.checked}
+		});
+	},
+	deleteThisTask(){
+		Tasks.remove(this.props.task._id);
+	},
 	render(){
+		// 添加下面一行的内容
+	    // 当任务被完成的时候给它们一个不同的class
+	    // 这样，通过CSS中的设置后，它们会看起来更好一些。
+	    const taskClassName = this.props.task.checked ? "checked" : "";
+		
 		return (
-			<li> {this.props.task.text}</li>
+			<li taskClassName={taskClassName}>
+				<button className='delete' onClick={this.deleteThisTask}>
+					&times;
+				</button>
+
+				<input
+					type='checkbox'
+					readOnly={true}
+					checked={this.props.task.checked}
+					onClick={this.toggleChecked} />
+				<span className='text'>{this.props.task.text}</span>
+			</li>
 		);
 	}
 });
