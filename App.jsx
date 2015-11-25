e8ad8b0a9e79ce58a422150f82d351f12567df74
Martin-zhz1994 <martinzh94@hotmail.com>
2015-11-25 12:44:56 +0800

    通过表单(form)添加

--- App.jsx
+++ App.jsx
@@ -6,7 +6,7 @@ App = React.createClass({
   	// 从Tasks集合中获取数据并添加到this.data中
   	getMeteorData(){
   		return {
-  			tasks:Tasks.find({}).fetch()
+  			tasks:Tasks.find({},{sort:{createdAt:-1}}).fetch()
   		}
   	},
 
@@ -24,14 +24,37 @@ App = React.createClass({
 			return <Task key={task._id} task={task} />;
 		});
 	},
+
+	handleSubmit(event) {
+		event.preventDefault();
+
+		// 通过React的ref属性找到输入框的值
+		var text = React.findDOMNode(this.refs.textInput).value.trim();
+
+		Tasks.insert({
+			text:text,
+			createdAt:new Date() // 当前时间
+		});
+
+		// 复原表单
+		React.findDOMNode(this.refs.textInput).value="";
+	},
+
 	render(){
 		return (
 			<div className='container'>
 				<header>
 					<h1>Todo List</h1>
 				</header>
-
-				<ul>
+				{/* 添加开始 */}
+    			<form className="new-task" onSubmit={this.handleSubmit} >
+		      	<input
+			        type="text"
+			        ref="textInput"
+			        placeholder="Type to add new tasks" />
+			    </form>
+			    {/* 添加开始 */}				
+			    <ul>
 					{this.renderTasks()}
 				</ul>
 			</div>
